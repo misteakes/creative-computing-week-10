e52b7a6f3e97e39d3139e3e0b232c375ea66bec1
Misty <misty@mistys-mbp.wireless.newschool.edu>
2018-10-30 10:18:56 -0400

    .

--- js/01_loops.js
+++ js/01_loops.js
@@ -60,7 +60,7 @@ shapes.forEach(function(shape, i) {
 
 	var percentage = i / 200 * 100;
 
-	var color = hslColor(i*2, percentage, 50);
+	var color = hslColor(i*2, i, 50);
 	shape.style.background = color;
 });
 
